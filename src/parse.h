7f80d40f491e104751827223c1f96b630aff7e4d
William Rusnack <williamrusnack@gmail.com>
2025-05-07 15:11:40 -0400

    fixed PARS_LINE
    underflow error from privious differing implementation of mGets
    Incorrect errorArg assignment

--- src/parse.h
+++ src/parse.h
@@ -2,22 +2,14 @@
 #define PARSE_H
 
 #define PARSE_LINE(errorId, numberOfParsedParameters, formatString, errorMessage, ...) \
+  parseBuf[PARSE_BUF_SIZE - 2] = '\0'; \
   s = mGets(parseBuf, PARSE_BUF_SIZE, patch); \
-  if (NULL == s) { \
-    PLM(patch, "Failed to get line when parsing."); \
-    ERROR_CHECK(FileError); \
-  } \
-  if (*(s - 1) != '\n') { \
-    errorArg.pathLine = (struct PathLine){ patch->path, patch->line - 1 }; \
-    ERROR_CHECK(ParseFail_BufferOverflow); \
-  } \
+  ERROR_CONDITION(errorId, NULL == s, PLM(patch, "Failed to get line when parsing.")); \
+  ERROR_CONDITION(ParseFail_BufferOverflow, '\0' != s[PARSE_BUF_SIZE - 2], errorArg.pathLine = ((struct PathLine){ patch->path, patch->line - 1 })); \
   i = sscanf(parseBuf, formatString, __VA_ARGS__); \
-  if (i < numberOfParsedParameters) { \
-    PLM(patch, errorMessage); \
-    ERROR_CHECK(errorId); \
-  }
+  ERROR_CONDITION(errorId, numberOfParsedParameters > i, PLM(patch, errorMessage))
 
 #define PLM(f, msg) \
-  errorArg.pathLineMsg = (struct PathLineMsg){ f->path, f->line - 1, msg }
+  errorArg.pathLineMsg = ((struct PathLineMsg){ f->path, f->line - 1, msg })
 
 #endif
